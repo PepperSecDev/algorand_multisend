d25a8ed4c2d86d60d85d3772cf0d274bc751ae96
Alexey <peretsmobil@gmail.com>
2019-06-30 21:19:23 +0300

    dummy airdrop

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 const algosdk = require('algosdk')
+const fs = require('fs')
 require('dotenv').config()
-const { SEED_PHRASE, TOKEN, ASERVER, APOST } = process.env
+const { SEED_PHRASE, TOKEN, ASERVER, APORT } = process.env
 
 const account = algosdk.mnemonicToSecretKey(SEED_PHRASE)
 console.log('Your address', account.addr)
 
+let txn = { 
+    to: null,
+    fee: 1,
+    amount: null,
+    firstRound: null,
+    lastRound: null,
+    genesisID: null,
+    genesisHash: null,
+    // closeRemainderTo: 'IDUTJEUIEVSMXTU4LGTJWZ2UE2E6TIODUKU6UW3FU3UKIQQ77RLUBBBFLA',
+    note: new Uint8Array(0) // new Uint8Array(Buffer.from('6gAVR0Nsv5Y=', 'base64'))
+};
 
 async function main() {
-
-    const algodclient = new algosdk.Algod(TOKEN, ASERVER, APOST);
-
+    let recipients = fs.readFileSync('./recipients.csv', 'utf8');
+    recipients = recipients.split('\n')
+    const algodclient = new algosdk.Algod(TOKEN, ASERVER, APORT);
     let params = await algodclient.getTransactionParams();
     let endRound = params.lastRound + parseInt(1000);
 
-    const txn = { 
-        "to": "7ZUECA7HFLZTXENRV24SHLU4AVPUTMTTDUFUBNBD64C73F3UHRTHAIOF6Q",
-        "fee": 10,
-        "amount": 847,
-        "firstRound": params.lastRound,
-        "lastRound": endRound,
-        "genesisID": params.genesisID,
-        "genesisHash": params.genesishashb64,
-        // "closeRemainderTo": "IDUTJEUIEVSMXTU4LGTJWZ2UE2E6TIODUKU6UW3FU3UKIQQ77RLUBBBFLA",
-        "note": new Uint8Array(0) // new Uint8Array(Buffer.from("6gAVR0Nsv5Y=", "base64"))
-    };
+    console.log('\nAirdrop started!')
+    for (let recipient of recipients) {
+        recipient = recipient.split(',')
+        txn.to = recipient[0]
+        txn.amount = Number(recipient[1])
+        txn.firstRound = params.lastRound
+        txn.lastRound = endRound
+        txn.genesisID = params.genesisID
+        txn.genesisHash = params.genesishashb64
 
-    const signedTxn = algosdk.signTransaction(txn, account.sk)
-    let tx = (await algodclient.sendRawTransaction(signedTxn.blob));
-    console.log("Transaction : " + tx.txId);
+        const signedTxn = algosdk.signTransaction(txn, account.sk)
+        let tx = (await algodclient.sendRawTransaction(signedTxn.blob));
+        console.log(`Transaction id for ${recipient[0]}: ` + tx.txId);
+
+        params = await algodclient.getTransactionParams();
+        endRound = params.lastRound + parseInt(1000);
+    }
+
+    console.log('You have successfully made the Algorand airdrop!')
 }
 
 main()
